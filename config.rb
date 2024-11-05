7b7fb60c2ba882174b2120cc53fbeefa3653a740
Venus B <venusshum@gmail.com>
2024-11-05 09:23:57 +0000

    change config to add relative path

--- config.rb
+++ config.rb
@@ -3,7 +3,9 @@ require 'govuk_tech_docs'
 GovukTechDocs.configure(self)
 
 set :layout, 'custom'
+set :css_dir, 'stylesheets'
+set :images_dir, "images"
 
-
-set :js_dir, 'javascripts'
-activate :asset_hash, :exts => %w(.jpg) # Only hash for .jpg
+configure :build do
+  activate :relative_assets
+end
